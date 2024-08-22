21f57390309be73aa1951f18e7419216383c8098
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-22 13:08:28 -0400

    Requirement for parent

--- ludu-engine/src/scene/component.ts
+++ ludu-engine/src/scene/component.ts
@@ -1,16 +1,23 @@
 import { generateUUID } from "../util";
+import { Entity } from "./entity";
 
 export abstract class Component {
 	private _id: number;
 	protected _type: string;
+	protected _parent: Entity;
 
-	protected constructor(type: string) {
+	protected constructor(type: string, parent: Entity) {
 		this._id = generateUUID();
 		this._type = type;
+		this._parent = parent;
 	}
 
 	public get id(): number {
 		return this._id;
+	}
+
+	public get parent(): Entity {
+		return this._parent;
 	}
 
 	public start(): void {}
