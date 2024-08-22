0967188df55ed16e529c6ec6b3d9d0cd17a5477b
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-22 12:17:05 -0400

    Id creation

--- ludu-engine/src/scene/component.ts
+++ ludu-engine/src/scene/component.ts
@@ -1,8 +1,16 @@
+import { generateUUID } from "../util";
+
 export abstract class Component {
+	private _id: number;
 	protected _type: string;
 
 	protected constructor(type: string) {
+		this._id = generateUUID();
 		this._type = type;
+	}
+
+	public get id(): number {
+		return this._id;
 	}
 
 	public start(): void {}
