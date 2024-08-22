301bc1009e6f0ce6360e7569c0b7ed0b274fb5df
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-22 13:08:20 -0400

    Create camera-component.ts

---
+++ ludu-engine/src/scene/components/camera-component.ts
@@ -0,0 +1,26 @@
+import { Camera, Renderer } from "../../renderer";
+import { PerspectiveCamera } from "../../renderer/perspective-camera";
+import { Component } from "../component";
+import { Entity } from "../entity";
+
+export class CameraComponent extends Component {
+	private _camera: Camera;
+
+	public constructor(parent: Entity) {
+		super("camera", parent);
+
+		let transformMatrix = this._parent.worldTransform.matrix;
+		this._camera = new PerspectiveCamera(45, 1, 1, 2000, transformMatrix);
+
+		// temp
+		Renderer.camera = this._camera;
+	}
+
+	public start() {}
+
+	public update() {
+		this._camera.transformMatrix = this._parent.worldTransform.matrix;
+	}
+
+	public render() {}
+}
