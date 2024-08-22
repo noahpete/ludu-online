237cc2a0e19865692b406e0431531dd0c927214d
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-22 12:17:37 -0400

    Local and world transform matrices

--- ludu-engine/src/scene/components/transform-component.ts
+++ ludu-engine/src/scene/components/transform-component.ts
@@ -2,24 +2,34 @@ import { Transform, Vector3 } from "../../math";
 import { Component } from "../component";
 
 export class TransformComponent extends Component {
-	private _transform: Transform;
+	private _localTransform: Transform;
+	private _worldTransform: Transform;
 
 	public constructor() {
 		super("transform");
 
-		this._transform = new Transform();
+		this._localTransform = new Transform();
+		this._worldTransform = new Transform();
+	}
+
+	public get localTransform(): Transform {
+		return this._localTransform;
+	}
+
+	public get worldTransform(): Transform {
+		return this._worldTransform;
 	}
 
 	public get position(): Vector3 {
-		return this._transform.position;
+		return this._localTransform.position;
 	}
 
 	public get rotation(): Vector3 {
-		return this._transform.rotation;
+		return this._localTransform.rotation;
 	}
 
 	public get scale(): Vector3 {
-		return this._transform.scale;
+		return this._localTransform.scale;
 	}
 
 	public start(): void {}
