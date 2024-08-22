30ee12bac3aa892f41e108c4ae276c628495eb7f
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-22 13:09:36 -0400

    Input transformMatrix in constructor

--- ludu-engine/src/renderer/camera.ts
+++ ludu-engine/src/renderer/camera.ts
@@ -2,14 +2,25 @@ import { Matrix4x4 } from "../math";
 
 export class Camera {
 	protected _projectionMatrix: Matrix4x4;
+	protected _transformMatrix: Matrix4x4;
 
-	protected constructor(projectionMatrix?: Matrix4x4) {
+	protected constructor(projectionMatrix?: Matrix4x4, transformMatrix?: Matrix4x4) {
 		this._projectionMatrix = projectionMatrix
 			? projectionMatrix
 			: Matrix4x4.orthographic(-1, 1, -1, 1, -1, 1);
+
+		this._transformMatrix = transformMatrix ? transformMatrix : Matrix4x4.identity();
 	}
 
 	public get projectionMatrix(): Matrix4x4 {
 		return this._projectionMatrix;
+	}
+
+	public get transformMatrix(): Matrix4x4 {
+		return this._transformMatrix;
+	}
+
+	public set transformMatrix(value: Matrix4x4) {
+		this._transformMatrix = value;
 	}
 }
--- ludu-engine/src/renderer/perspective-camera.ts
+++ ludu-engine/src/renderer/perspective-camera.ts
@@ -2,7 +2,13 @@ import { Matrix4x4 } from "../math";
 import { Camera } from "./camera";
 
 export class PerspectiveCamera extends Camera {
-	public constructor(fov: number, aspect: number, near: number, far: number) {
-		super(Matrix4x4.perspective(fov, aspect, near, far));
+	public constructor(
+		fov: number,
+		aspect: number,
+		near: number,
+		far: number,
+		transformMatrix?: Matrix4x4
+	) {
+		super(Matrix4x4.perspective(fov, aspect, near, far), transformMatrix);
 	}
 }
