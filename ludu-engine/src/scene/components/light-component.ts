999fa5ab52bdc28f3da384d064caadcf805bc6fd
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-29 11:13:11 -0400

    Added base light component

--- ludu-engine/src/scene/components/light-component.ts
+++ ludu-engine/src/scene/components/light-component.ts
@@ -1,16 +1,37 @@
 import { Camera } from "../../renderer";
+import { PointLight } from "../../renderer/point-light";
 import { Component } from "../component";
 import { Entity } from "../entity";
 import { Scene } from "../scene";
 
 export class LightComponent extends Component {
-	public constructor(parent: Entity) {
+	private _light: PointLight;
+
+	public constructor(parent: Entity, data?: any) {
 		super("light", parent);
+
+		if (data?.type === "point") {
+			this._light = new PointLight();
+			// } else if (data.type === "directional") {
+			// } else if (data.type === "spot") {
+		} else {
+			this._light = new PointLight();
+		}
+
+		if (data?.color) {
+			this._light.ambient = data.color;
+			this._light.diffuse = data.color;
+			this._light.specular = data.color;
+		}
+	}
+
+	public get light(): PointLight {
+		return this._light;
 	}
 
 	public start(): void {}
 
-	public update(): void {}
+	public update(dt: number): void {}
 
 	public render(camera: Camera, scene: Scene): void {}
 }
