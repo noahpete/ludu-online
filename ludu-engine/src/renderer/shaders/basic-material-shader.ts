d51b87c35a457ff270b9ee86a7ccfe805c0732d2
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-30 10:56:59 -0400

    Added texture coords

--- ludu-engine/src/renderer/shaders/basic-material-shader.ts
+++ ludu-engine/src/renderer/shaders/basic-material-shader.ts
@@ -4,9 +4,11 @@ const vertexSource = `#version 300 es
 
     layout (location = 0) in vec3 a_position;
 	layout (location = 1) in vec3 a_normal;
+	layout (location = 2) in vec2 a_texcoord;
 
 	out vec3 v_fragPosition;
 	out vec3 v_normal;
+	out vec2 v_texcoord;
 	
 	uniform mat4 u_model;
 	uniform mat4 u_view;
@@ -15,6 +17,7 @@ const vertexSource = `#version 300 es
 	void main() {
 		v_fragPosition = vec3(u_model * vec4(a_position, 1.0));
 		v_normal = mat3(transpose(inverse(u_model))) * a_normal;
+		v_texcoord = a_texcoord;
 		gl_Position = u_projection * u_view * vec4(v_fragPosition, 1.0);
 	}
 `;
@@ -43,6 +46,7 @@ const fragmentSource = `#version 300 es
 
 	in vec3 v_fragPosition;
 	in vec3 v_normal;
+	in vec2 v_texcoord;
 
 	#define MAX_NUMBER_LIGHTS 32
 
@@ -50,6 +54,7 @@ const fragmentSource = `#version 300 es
 	uniform int u_pointLightsCount;
 	uniform vec3 u_viewPosition;
 	uniform Material u_material;
+	uniform sampler2D u_texture;
 
 	out vec4 outColor;
 
@@ -59,7 +64,7 @@ const fragmentSource = `#version 300 es
 		vec3 normal = normalize(v_normal);
 		vec3 viewDir = normalize(u_viewPosition - v_fragPosition);
 
-		vec3 result = vec3(0.0);
+		vec3 result = vec3(0.5) * vec3(texture(u_texture, v_texcoord));
 
 		for (int i = 0; i < u_pointLightsCount; i++) {
 			PointLight light = u_pointLights[i];
@@ -67,22 +72,24 @@ const fragmentSource = `#version 300 es
 		}
 
 		outColor = vec4(result, 1.0);
+		// outColor = texture(u_texture, v_texcoord);
 	}
 
 	vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
 		vec3 lightDir = normalize(light.position - fragPos);
+		vec3 texture = vec3(texture(u_texture, v_texcoord));
 
 		// ambient
-		vec3 ambient = light.ambient * u_material.diffuse;
+		vec3 ambient = light.ambient * u_material.diffuse * texture;
 
 		// diffuse
 		float diff = max(dot(normal, lightDir), 0.0);
-		vec3 diffuse = light.diffuse * diff * u_material.diffuse;
+		vec3 diffuse = light.diffuse * diff * u_material.diffuse * texture;
 
 		// specular
 		vec3 reflectDir = reflect(-lightDir, normal);
 		float spec = pow(max(dot(viewDir, reflectDir), 0.0), u_material.shininess); // shininess
-		vec3 specular = light.specular * spec * u_material.specular;
+		vec3 specular = light.specular * spec * u_material.specular * texture;
 
 		// attenuation
 		float distance = length(light.position - fragPos);
@@ -94,7 +101,6 @@ const fragmentSource = `#version 300 es
 		specular *= attenuation;
 
 		return (ambient + diffuse + specular);
-		return (ambient + diffuse);
 	}
 `;
 
