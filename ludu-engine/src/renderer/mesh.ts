1c22c694c3013d521d89696778c2a4ecb7180855
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-08-30 10:57:19 -0400

    Update to provide texture coords

--- ludu-engine/src/renderer/mesh.ts
+++ ludu-engine/src/renderer/mesh.ts
@@ -10,12 +10,16 @@ export class Mesh {
 	private _normalVBO: WebGLBuffer;
 	private _normalCount: number;
 
-	public constructor(positions: number[], normals: number[]) {
+	private _texcoordVBO: WebGLBuffer;
+	private _texcoordCount: number;
+
+	public constructor(positions: number[], normals: number[], texcoords: number[]) {
 		this._VAO = gl.createVertexArray() as WebGLVertexArrayObject;
 		gl.bindVertexArray(this._VAO);
 
 		this._positionCount = positions.length;
 		this._normalCount = normals.length;
+		this._texcoordCount = texcoords.length;
 
 		this._positionVBO = gl.createBuffer() as WebGLBuffer;
 		gl.bindBuffer(gl.ARRAY_BUFFER, this._positionVBO);
@@ -28,6 +32,12 @@ export class Mesh {
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
 		gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 3 * 4, 0);
 		gl.enableVertexAttribArray(1);
+
+		this._texcoordVBO = gl.createBuffer() as WebGLBuffer;
+		gl.bindBuffer(gl.ARRAY_BUFFER, this._texcoordVBO);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
+		gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(2);
 
 		gl.bindVertexArray(null);
 	}
