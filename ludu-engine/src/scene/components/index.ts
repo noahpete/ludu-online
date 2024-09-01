5b23f19f00c3fab5057531735fec2127c8b0e1d1
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-09-01 12:33:34 -0400

    Update exports

--- ludu-engine/src/scene/components/index.ts
+++ ludu-engine/src/scene/components/index.ts
@@ -1,1 +1,4 @@
 export { TransformComponent } from "./transform-component";
+export { CameraComponent } from "./camera-component";
+export { LightComponent } from "./light-component";
+export { ModelComponent } from "./model-component";
