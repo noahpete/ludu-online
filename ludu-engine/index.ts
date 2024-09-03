aa12b6c1965d2b4b2ca91eb2c853c3d102a5ec36
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-09-03 13:20:02 -0400

    Changed index.ts export

---
+++ ludu-engine/index.ts
@@ -0,0 +1,5 @@
+export * from "./src/renderer";
+export * from "./src/util";
+export * from "./src/core";
+export * from "./src/scene";
+export * from "./src/math";
--- ludu-engine/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./renderer";
-export * from "./util";
-export * from "./core";
-export * from "./scene";
-export * from "./math";
