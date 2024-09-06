e5ea802cc6a2e27c82525d5013ecf748773fc0ab
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-09-06 12:11:14 -0400

    Update GameContext.tsx

--- ludu-editor/src/contexts/GameContext.tsx
+++ ludu-editor/src/contexts/GameContext.tsx
@@ -5,6 +5,8 @@ import * as ld from "ludu-engine";
 interface GameContextType {
 	app: ld.Application | null;
 	setApp: Dispatch<SetStateAction<ld.Application | null>>;
+	selectedEntity: ld.Entity | null;
+	setSelectedEntity: Dispatch<SetStateAction<ld.Entity | null>>;
 }
 
 const GameContext = createContext<GameContextType | null>(null);
